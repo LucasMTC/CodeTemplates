7092023b7a99e5e9a54d84668415747689ac51bc
LucasMTC <lucas.callisperis@gmail.com>
2025-07-11 08:52:19 -0600

    Added Topological Sort

--- Graph/TopologicalSort.py
+++ Graph/TopologicalSort.py
@@ -5,14 +5,35 @@ def create_graph(connections:list) -> dict:
     for con in connections:
         start, end = con[0], con[1]
         graph[start].append(end)
+        if end not in graph:
+            graph[end] = []
     return graph
 
 def find_indegree(graph:dict) -> dict:
-    indegree = defaultdict(int)
+    indegree = {node:0 for node in graph.keys()}
     for list in graph.values():
         for node in list:
             indegree[node] += 1
     return indegree
+
+def topological_sort(graph:dict, indegree:dict) -> list:
+    queue = deque()
+    visited = set()
+    ans = []
+    for key, value in indegree.items():
+        if value == 0:
+            queue.append(key)
+            visited.add(key)
+    
+    while queue:
+        curr = queue.popleft()
+        ans.append(curr)
+        for node in graph[curr]:
+            indegree[node] -= 1
+            if node not in visited and indegree[node] == 0:
+                queue.append(node)
+                visited.add(node)
+    return ans
 
 
 if __name__ == "__main__":
@@ -31,6 +52,5 @@ if __name__ == "__main__":
   ["I", "J"]
 ]
     graph = create_graph(nodes)
-    print(graph)
     indegree = find_indegree(graph)
-    print(indegree)
+    print(topological_sort(graph, indegree))
